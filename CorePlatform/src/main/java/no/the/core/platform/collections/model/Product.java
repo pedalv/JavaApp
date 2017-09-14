9f312483e03998689ef94c6828d4fd755c8634e9
pedroaalves <pedro.alves@innovatec.no>
2017-09-14 16:31:59 +0200

    Java Core Platform

---
+++ CorePlatform/src/main/java/no/the/core/platform/collections/model/Product.java
@@ -0,0 +1,33 @@
+package no.the.core.platform.collections.model;
+
+public class Product {
+
+    private final String name;
+    private final int weight;
+
+    public Product(String name, int weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getWeight()
+    {
+        return weight;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+}
