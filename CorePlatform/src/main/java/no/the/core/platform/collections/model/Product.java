0c4047a4b02b3e09dba772cf385c1252e2370c19
pedroaalves <pedro.alves@innovatec.no>
2017-09-15 10:11:57 +0200

    Java Cor Platform

--- CorePlatform/src/main/java/no/the/core/platform/collections/model/Product.java
+++ CorePlatform/src/main/java/no/the/core/platform/collections/model/Product.java
@@ -1,6 +1,25 @@
 package no.the.core.platform.collections.model;
 
+import java.util.Comparator;
+
+import static java.util.Comparator.comparing;
+
 public class Product {
+
+    /* Java 7
+    public static final Comparator<Product> BY_WEIGHT
+            = new Comparator<Product>()
+    {
+        public int compare(final Product p1, final Product p2)
+        {
+            return Integer.compare(p1.getWeight(), p2.getWeight());
+        }
+    };
+    */
+
+    /* Java 8 */
+    public static final Comparator<Product> BY_WEIGHT
+            = comparing(Product::getWeight);
 
     private final String name;
     private final int weight;
