74783d979d3687d2320194fb4c9a5142940ac5cc
pedroaalves <pedro.alves@innovatec.no>
2017-09-22 15:06:57 +0200

    Java Core Platform

---
+++ CorePlatform/src/test/java/no/the/core/platform/generics/why/CircularBufferTest.java
@@ -0,0 +1,44 @@
+package no.the.core.platform.generics.why;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircularBufferTest
+{
+    private CircularBuffer buffer = new CircularBuffer(2);
+
+    @Test
+    public void shouldOfferPollableElement()
+    {
+        assertTrue(buffer.offer(1));
+
+        assertEquals(1, buffer.poll());
+        assertNull(buffer.poll());
+    }
+
+    @Test
+    public void shouldNotOfferWhenBufferIsFull()
+    {
+        assertTrue(buffer.offer(1));
+        assertTrue(buffer.offer(2));
+        assertFalse(buffer.offer(3));
+    }
+
+    @Test
+    public void shouldNotPollWhenBufferIsEmpty()
+    {
+        assertNull(buffer.poll());
+    }
+
+    @Test
+    public void shouldRecycleBuffer()
+    {
+        assertTrue(buffer.offer(1));
+        assertTrue(buffer.offer(2));
+        assertEquals(1, buffer.poll());
+        assertTrue(buffer.offer(3));
+        assertEquals(2, buffer.poll());
+        assertEquals(3, buffer.poll());
+    }
+}
