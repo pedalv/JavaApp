5f364da96b38e08b38aef74a7e2a8a2dd4c208e1
Pedro Alves <alves.pedro0@gmail.com>
2018-07-30 14:21:07 +0200

    OCP

--- OCP/OCP/source/src/main/java/no/book/chapter1AdvancedClassDesign/creatingnestedclasses/memberinnerclass/CaseOfThePrivateInterface.java
+++ OCP/OCP/source/src/main/java/no/book/chapter1AdvancedClassDesign/creatingnestedclasses/memberinnerclass/CaseOfThePrivateInterface.java
@@ -2,12 +2,22 @@ package no.book.chapter1AdvancedClassDesign.creatingnestedclasses.memberinnercla
 
 public class CaseOfThePrivateInterface {
 
+    /*
+    The rule that all methods in an interface are public still applies.
+
+    The interface itself does not have to be public, though.
+    Just like any inner class, an inner interface can be private.
+    This means that the interface can only be referred to within the current outer class.
+     */
     private interface Secret {
         public void shh();
     }
 
+    /*
+    A class that implements the interface must define that method as public.
+     */
     class DontTell implements Secret {
         public void shh() { }
     }
-
+    
 }
