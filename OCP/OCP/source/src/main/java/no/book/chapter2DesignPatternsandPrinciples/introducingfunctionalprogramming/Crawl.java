5827cbbc194a9c591be289bf8595529f36e65576
Pedro Alves <alves.pedro0@gmail.com>
2018-07-31 19:53:27 +0200

    OCP

---
+++ OCP/OCP/source/src/main/java/no/book/chapter2DesignPatternsandPrinciples/introducingfunctionalprogramming/Crawl.java
@@ -0,0 +1,14 @@
+package no.book.chapter2DesignPatternsandPrinciples.introducingfunctionalprogramming;
+
+/*
+Error: java: Unexpected @FunctionalInterface annotation
+  no.book.chapter2DesignPatternsandPrinciples.introducingfunctionalprogramming.Crawl is not a functional interface
+    multiple non-overriding abstract methods found in interface no.book.chapter2DesignPatternsandPrinciples.introducingfunctionalprogramming.Crawl
+Finally, the Crawl method defines
+two abstract methods; therefore it cannot be a functional interface.
+ */
+//@FunctionalInterface
+public interface Crawl {
+    public void crawl();
+    public int getCount();
+}
