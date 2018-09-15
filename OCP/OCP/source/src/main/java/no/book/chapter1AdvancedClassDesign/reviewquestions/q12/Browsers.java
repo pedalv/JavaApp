74f78a2d936b9bf68683ba49cb9ea02b8de79a8c
Pedro Alves <alves.pedro0@gmail.com>
2018-09-15 14:19:00 +0200

    OCP

--- OCP/OCP/source/src/main/java/no/book/chapter1AdvancedClassDesign/reviewquestions/q12/Browsers.java
+++ OCP/OCP/source/src/main/java/no/book/chapter1AdvancedClassDesign/reviewquestions/q12/Browsers.java
@@ -9,7 +9,7 @@ public class Browsers {
     }
     
     static class Firefox extends Browser {
-        public void go() {
+        @Override public void go() {
             System.out.println("Inside Firefox");
         }
     }
@@ -22,7 +22,33 @@ public class Browsers {
 
     public static void main(String[] args) {
         Browser b = new Firefox();
-        IE e = (IE) b;
-        e.go();
+        b.go();
+
+        //IE e = b;
+        /*
+        Error: java: incompatible types:
+            no.book.chapter1AdvancedClassDesign.reviewquestions.q12.Browsers.Browser
+            cannot be converted to no.book.chapter1AdvancedClassDesign.reviewquestions.q12.Browsers.IE
+         */
+
+        //IE e = (IE) b;
+        /*
+        Exception in thread "main"
+            java.lang.ClassCastException: no.book.chapter1AdvancedClassDesign.reviewquestions.q12.Browsers$Firefox
+            cannot be cast to no.book.chapter1AdvancedClassDesign.reviewquestions.q12.Browsers$IE
+	        at no.book.chapter1AdvancedClassDesign.reviewquestions.q12.Browsers.main(Browsers.java)
+
+         */
+        //e.go();
     }
 }
+
+/*
+What is the result of the following code?
+
+    A. Inside Browser
+    B. Inside Firefox
+    C. Inside IE
+        D. The code does not compile.
+E. A runtime exception is thrown.
+ */
