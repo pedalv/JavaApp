8ba09d720b771327d5a8d39b43d266c8026557ff
Pedro Alves <alves.pedro0@gmail.com>
2018-09-16 14:39:32 +0200

    OCP

---
+++ OCP/OCP/source/src/main/java/no/book/chapter2DesignPatternsandPrinciples/reviewquestions/q05/SingletonPatterns.java
@@ -0,0 +1,17 @@
+package no.book.chapter2DesignPatternsandPrinciples.reviewquestions.q05;
+
+public class SingletonPatterns {
+    /*
+    What are some of the properties of using the singleton pattern? (Choose all that apply.)
+
+        A. Singleton object can be replaced with encapsulated setter method.
+    B. Requires constructor of singleton class to be private.
+        C. Singleton object must be named instance.
+        D. Singleton object may be private or protected.
+    E. Ensures that there is only one instance of an object in memory.
+    F. Requires a public static method to retrieve the instance of the singleton.
+
+     ANSWER
+
+     */
+}
