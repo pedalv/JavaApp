72b3c67ff081269f73e662b8278136479760110c
Pedro Alves <alves.pedro0@gmail.com>
2018-09-16 16:21:43 +0200

    OCP

---
+++ OCP/OCP/source/src/main/java/no/book/chapter2DesignPatternsandPrinciples/reviewquestions/q19/SingletonPattern.java
@@ -0,0 +1,21 @@
+package no.book.chapter2DesignPatternsandPrinciples.reviewquestions.q19;
+
+public class SingletonPattern {
+
+    /*
+    Which three scenarios would best benefit from using a singleton pattern?
+    (Choose all three.)
+
+    A. Create read‐only objects that are thread‐safe.
+        B. Manage a reusable cache of objects.
+    C. Ensure that all objects are lazily instantiated.
+        D. Manage write access to a log file.
+        E. Provide central access to application configuration data.
+        F. Allow multiple instances of a static object to be managed in memory.
+
+    ANSWER
+
+    
+
+     */
+}
