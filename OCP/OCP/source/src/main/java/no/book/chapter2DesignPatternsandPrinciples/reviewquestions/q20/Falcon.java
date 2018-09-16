83b4f27c0bfdfc6336207440f7e2d62410d09c08
Pedro Alves <alves.pedro0@gmail.com>
2018-09-16 16:26:01 +0200

    OCP

---
+++ OCP/OCP/source/src/main/java/no/book/chapter2DesignPatternsandPrinciples/reviewquestions/q20/Falcon.java
@@ -0,0 +1,32 @@
+package no.book.chapter2DesignPatternsandPrinciples.reviewquestions.q20;
+
+public class Falcon implements CanFly, HasWings { // Line 7
+
+    // TODO
+    @Override
+    public void fly() {
+
+    }
+
+    // TODO
+    @Override
+    public Object getWingSpan() {
+        return null;
+    }
+
+} // Line 8
+
+/*
+Choose the correct statement about the following code:
+
+    A. It compiles without issue.
+    B. The code will not compile because of line 2.
+    C. The code will not compile because of line 4.
+    D. The code will not compile because of line 5.
+    E. The code will not compile because of lines 2 and 5.
+F. The code will not compile because the class Falcon doesn’t implement the interface methods.
+
+ANSWER
+
+
+ */
