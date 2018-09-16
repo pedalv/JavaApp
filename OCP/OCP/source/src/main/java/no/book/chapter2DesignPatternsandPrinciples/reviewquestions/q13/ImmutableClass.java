2640454b247821319558b29d04cc1655e18d907f
Pedro Alves <alves.pedro0@gmail.com>
2018-09-16 15:31:20 +0200

    OCP

---
+++ OCP/OCP/source/src/main/java/no/book/chapter2DesignPatternsandPrinciples/reviewquestions/q13/ImmutableClass.java
@@ -0,0 +1,21 @@
+package no.book.chapter2DesignPatternsandPrinciples.reviewquestions.q13;
+
+public class ImmutableClass {
+    /*
+    Which of the following are properties of classes that define immutable objects?
+    (Choose all that apply.)
+
+        A. They don’t define any getter methods.
+    B. All of the instance variables marked private and final.
+    C. They don’t define any setter methods.
+    D. They mark all instance variables static.
+        E. They prevent methods from being overridden.
+    F. All getter methods are marked synchronized.
+
+    ANSWER
+    
+
+
+
+     */
+}
