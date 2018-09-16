e363f259d08efa10524fa651bee688028ff528cd
Pedro Alves <alves.pedro0@gmail.com>
2018-09-16 14:49:11 +0200

    OCP

---
+++ OCP/OCP/source/src/main/java/no/book/chapter2DesignPatternsandPrinciples/reviewquestions/q08/QuestionsomInterfaces.java
@@ -0,0 +1,19 @@
+package no.book.chapter2DesignPatternsandPrinciples.reviewquestions.q08;
+
+public class QuestionsomInterfaces {
+    /*
+    Which of the following are true of interfaces? (Choose all that apply.)
+
+    A. They can extend other classes.
+        B. They cannot be extended.
+        C. They enable classes to have multiple inheritance.
+    D. They can only contain abstract methods.
+        E. They can be declared final.
+    F. All members of an interface are public.
+
+    answer
+
+    
+
+     */
+}
