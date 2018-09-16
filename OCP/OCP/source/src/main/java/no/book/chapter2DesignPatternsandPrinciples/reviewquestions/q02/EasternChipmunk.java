6439eef41bfd374e62e4f8794d2d180aecc3383c
Pedro Alves <alves.pedro0@gmail.com>
2018-09-16 14:05:04 +0200

    OCP

---
+++ OCP/OCP/source/src/main/java/no/book/chapter2DesignPatternsandPrinciples/reviewquestions/q02/EasternChipmunk.java
@@ -0,0 +1,28 @@
+package no.book.chapter2DesignPatternsandPrinciples.reviewquestions.q02;
+
+public class EasternChipmunk extends Chipmunk { // Line 8
+
+    public void chew() { System.out.println("Eastern Chipmunk is Chewing"); } // Line 9
+
+    //TODO
+    @Override
+    public void climb() {
+
+    }
+
+} // Line 10
+
+/*
+What is the result of the following code?
+
+    A. It compiles and runs without issue.
+    B. The code will not compile because of line 2.
+    C. The code will not compile because of line 4.
+    D. The code will not compile because of line 5.
+E. The code will not compile because of line 8.
+    F. It compiles but throws an exception at runtime.
+
+ANSWER:
+
+
+ */
