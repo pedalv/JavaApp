01614725ab8772a6706aaeffee15d137b6a2f2f2
Pedro Alves <alves.pedro0@gmail.com>
2019-05-04 16:34:40 +0200

    Kotlin

--- Kotlin/src/main/java/no/agitec/fagaften/mars/kotlin/section05/imports/anotherpackage/AnotherFile.kt
+++ Kotlin/src/main/java/no/agitec/fagaften/mars/kotlin/section05/imports/anotherpackage/AnotherFile.kt
@@ -2,14 +2,17 @@ package no.agitec.fagaften.mars.kotlin.section05.imports.anotherpackage
 
 import no.agitec.fagaften.mars.kotlin.section05.imports.communications.Department.IT
 import no.agitec.fagaften.mars.kotlin.section05.imports.communications.Department.SALES
+import no.agitec.fagaften.mars.kotlin.section05.imports.communications.topLevel
 import no.agitec.fagaften.mars.kotlin.section05.imports.communications.CompanyCommunications as Comm
 import no.agitec.fagaften.mars.kotlin.section05.imports.communications.topLevel as tp
 import no.agitec.fagaften.mars.kotlin.section05.imports.communications.upperFirstAndLast as ufal
 
 
 fun main(args: Array<String>) {
+    topLevel("Hello from AnotherFile")
+    println(" --1.1-- ")
     tp("Hello from AnotherFile")
-    println(" --1-- ")
+    println(" --1.2-- ")
     println(Comm.getCopyrightLine())
     println(" --2-- ")
     println(IT.getDeptInfo())
