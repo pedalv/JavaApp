647d5fb4c43ac39e234ae16d5269a32699dbba85
Pedro Alves <alves.pedro0@gmail.com>
2019-05-04 12:15:51 +0200

    Kotlin

---
+++ Kotlin/src/main/java/no/agitec/fagaften/mars/kotlin/section05/object/companion/javacode/SomeClass.java
@@ -0,0 +1,15 @@
+package no.agitec.fagaften.mars.kotlin.section05.object.companion.javacode;
+
+public class SomeClass {
+
+    private static int privateVar = 6;
+
+    public static void main(String[] args) {
+        new SomeOtherClass().someOtherMethod();
+    }
+
+    public static void accessPrivateVar() {
+        System.out.println("I'm accessing privateVar: " + privateVar);
+    }
+
+}
