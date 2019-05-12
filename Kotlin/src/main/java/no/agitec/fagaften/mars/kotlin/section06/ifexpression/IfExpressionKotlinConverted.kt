f1ab1b7efa016a09549809b071d81c012f5d4650
Pedro Alves <alves.pedro0@gmail.com>
2019-05-12 17:15:17 +0200

    Kotlin

--- Kotlin/src/main/java/no/agitec/fagaften/mars/kotlin/section06/ifexpression/IfExpressionKotlinConverted.kt
+++ Kotlin/src/main/java/no/agitec/fagaften/mars/kotlin/section06/ifexpression/IfExpressionKotlinConverted.kt
@@ -1,6 +1,7 @@
 package no.agitec.fagaften.mars.kotlin.section06.ifexpression
 
-object IfExpressionJava {
+//object IfExpressionJava {
+object IfExpressionKotlinConverted {
 
     @JvmStatic
     fun main(args: Array<String>) {
