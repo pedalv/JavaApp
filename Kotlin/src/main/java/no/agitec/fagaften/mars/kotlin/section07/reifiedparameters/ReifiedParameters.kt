37f4d1355b9b8b099ac63d91d5b3a9a33fecc41e
Pedro Alves <alves.pedro0@gmail.com>
2019-06-06 19:15:30 +0200

    Kotlin

---
+++ Kotlin/src/main/java/no/agitec/fagaften/mars/kotlin/section07/reifiedparameters/ReifiedParameters.kt
@@ -0,0 +1,56 @@
+package no.agitec.fagaften.mars.kotlin.section07.reifiedparameters
+
+import java.math.BigDecimal
+
+fun main(args: Array<String>) {
+
+    val mixedList: List<Any> = listOf(
+            "string",
+            1,
+            BigDecimal(22.5),
+            "fall",
+            BigDecimal(-5938.393849))
+    //val bigDecimalsOnly = getElementsOfType<String>(mixedList)
+    //string
+    //fall
+    val bigDecimalsOnly = getElementsOfType<BigDecimal>(mixedList)
+    //22.5
+    //-5938.393849000000045634806156158447265625
+    for (item in bigDecimalsOnly) {
+        println(item)
+    }
+}
+
+/**/inline fun </**/reified T> getElementsOfType(list: List<Any>): List<T> {
+
+    var newList: MutableList<T> = mutableListOf()
+    for (element in list) {
+        if (element is T) {
+            newList.add(element)
+        }
+    }
+
+    return newList
+}
+/*
+Error: Kotlin: Cannot check for instance of erased type: T
+
+Solution:
+1. inline
+2. reified
+ */
+
+
+//class samethings<reified T>
+/*
+Error: Kotlin: Only type parameters of inline functions can be reified
+ */
+
+//val c = T()
+/*
+Error: Kotlin: Type parameter T cannot be called as function
+ */
+
+
+
+
