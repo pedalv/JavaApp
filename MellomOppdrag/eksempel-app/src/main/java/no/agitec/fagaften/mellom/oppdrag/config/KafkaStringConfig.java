72ed7c7210fb5e9b471636e4d63539bda83df815
Pedro Alves <alves.pedro0@gmail.com>
2020-08-27 12:00:12 +0200

    Kafka

---
+++ MellomOppdrag/eksempel-app/src/main/java/no/agitec/fagaften/mellom/oppdrag/config/KafkaStringConfig.java
@@ -0,0 +1,36 @@
+package no.agitec.fagaften.mellom.oppdrag.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.SimpleKafkaHeaderMapper;
+import org.springframework.kafka.support.converter.MessagingMessageConverter;
+
+@Configuration
+@Slf4j
+public class KafkaStringConfig {
+
+    /*******************************************************************************************************************
+     STRING SAMPLE: no.agitec.fagaften.mellom.oppdrag.kafka.spring.boot.string.sample
+     ******************************************************************************************************************/
+
+    @Bean
+    public NewTopic javaInUseTopic() {
+        return new NewTopic("java_in_use_topic", 1, (short) 1);
+    }
+
+    /**
+     * KafkaListener
+     *
+     * @return
+     */
+    @Bean // not required if Jackson is on the classpath
+    public MessagingMessageConverter simpleMapperConverter() {
+        MessagingMessageConverter messagingMessageConverter = new MessagingMessageConverter();
+        messagingMessageConverter.setHeaderMapper(new SimpleKafkaHeaderMapper());
+        return messagingMessageConverter;
+    }
+
+
+}
