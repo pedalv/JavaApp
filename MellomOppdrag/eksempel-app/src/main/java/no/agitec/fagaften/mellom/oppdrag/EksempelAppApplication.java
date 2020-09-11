45557a1b744af6c16afe11cbf41e447a56aba37b
Pedro Alves <alves.pedro0@gmail.com>
2020-09-11 09:57:26 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/java/no/agitec/fagaften/mellom/oppdrag/EksempelAppApplication.java
+++ MellomOppdrag/eksempel-app/src/main/java/no/agitec/fagaften/mellom/oppdrag/EksempelAppApplication.java
@@ -9,6 +9,18 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
+/**
+ * SpringBootApplication === @Configuration + @EnableAutoConfiguration + @ComponentScan
+ *
+ * @SpringBootApplication is a convenience annotation that adds all of the following:
+ *
+ * @Configuration: Tags the class as a source of bean definitions for the application context.
+ *
+ * @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings. For example, if spring-webmvc is on the classpath, this annotation flags the application as a web application and activates key behaviors, such as setting up a DispatcherServlet.
+ *
+ * @ComponentScan: Tells Spring to look for other components, configurations, and services in the com/example package, letting it find the controllers.
+ *
+ */
 @SpringBootApplication
 @Slf4j
 public class EksempelAppApplication {
