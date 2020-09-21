11bec1c7f1038c6f7324490d10b37dc412a9c4dc
Pedro Alves <alves.pedro0@gmail.com>
2020-09-21 09:27:54 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app-gradle/src/test/java/no/agitec/fagaften/mellom/oppdrag/EksempelAppApplicationTests.java
+++ MellomOppdrag/eksempel-app-gradle/src/test/java/no/agitec/fagaften/mellom/oppdrag/EksempelAppApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 //@RunWith(SpringRunner.class)
+//https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-testing-spring-boot-applications
 @SpringBootTest
-class EksempelAppApplicationTests {
+public class EksempelAppApplicationTests {
 
 	@Test
 	void contextLoads() {
@@ -16,6 +17,8 @@ class EksempelAppApplicationTests {
 }
 
 /*
-http://www.slf4j.org/codes.html#multiple_bindings
+Tips og Råd
 
+http://www.slf4j.org/codes.html#multiple_bindings
+https://stackoverflow.com/questions/30792268/loggerfactory-is-not-a-logback-loggercontext-but-logback-is-on-the-classpath
  */
