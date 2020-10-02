2ec71625d18b1ad638049a60b58481184f55cf87
Pedro Alves <alves.pedro0@gmail.com>
2020-10-02 16:13:21 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/nav/nav.component.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/nav/nav.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  isLogin : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signOut(){
+    console.log("Todo signOut");
   }
 
 }
