8cb125e18a2a3bf5275548cadc9586d36c5d4dd8
Pedro Alves <alves.pedro0@gmail.com>
2020-10-02 16:42:49 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/login/login.component.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/login/login.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  hasError : boolean = false;
+  hasLogout: boolean = false
+  error: String = "Invalid username and password."
+  logout: String = "You have been logged out."
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signIn(){
+    console.log("Todo signIn");
+    //https://dzone.com/articles/angular-logging-and-log-back
+
+    //_csrf=7f083167-fe48-4946-b14c-f1c3c0a9b568&username=bla&password=bla
+    //        07144555-7c61-4b51-9885-3c51caf0f349
+    //http://localhost:8080/login?username=bla&password=bla
+
   }
 
 }
