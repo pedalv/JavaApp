f0db2f2c8b1265a0e6f51dba0844f355f8003f0e
Pedro Alves <alves.pedro0@gmail.com>
2020-10-02 10:22:48 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/app-routing.module.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import { CommonModule } from '@angular/common';
-//import { FormsModule } from '@angular/forms';
+
 import { PartnerComponent } from './partner/partner.component';
 import { CustomerComponent } from './customer/customer.component';
 import { EmployeeComponent } from './employee/employee.component';
--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/app.module.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PartnerComponent } from './partner/partner.component';
+import { CustomerComponent } from './customer/customer.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartnerComponent,
+    CustomerComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
