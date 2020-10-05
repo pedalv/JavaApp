95fc6ee4d8e059a87421c6c75a84532db128a744
Pedro Alves <alves.pedro0@gmail.com>
2020-10-05 15:02:18 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/kafka/kafka.component.spec.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/kafka/kafka.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { KafkaService } from '../services/kafka.service';
 import { KafkaComponent } from './kafka.component';
 
 describe('KafkaComponent', () => {
+  let kafkaServiceStub: Partial<KafkaService>;
   let component: KafkaComponent;
   let fixture: ComponentFixture<KafkaComponent>;
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      declarations: [ KafkaComponent ]
+      declarations: [ KafkaComponent ],
+      providers: [ {provide: KafkaService, useValue: KafkaService}]
     })
     .compileComponents();
   });
