0e550ae6302f4f6e50722db9434a600403383ff7
Pedro Alves <alves.pedro0@gmail.com>
2020-10-06 12:59:41 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/services/employee.service.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/services/employee.service.ts
@@ -39,7 +39,6 @@ export class EmployeeService {
     return this.httpClient.delete(`${this.apiURL}/employees/${id}`);
   }
 
-/*
   public getFirstPage(){
       return this.httpClient.get<Employee[]>('${this.apiURL}/employees?_page=1',{ observe: 'response' })
         .pipe(tap(res => {
@@ -50,12 +49,40 @@ export class EmployeeService {
           this.next   = Link["next"];
         }));
   }
-*/
+
+  parse_link_header(header) {
+    if (header.length == 0) {
+      return ;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach( p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+    return links;
+  }
+
+  public getNextPage(url: string){
+        return this.httpClient.get<Employee[]>(url,{ observe: 'response' }).pipe(tap(res => {
+          const Link  = this.parse_link_header(res.headers.get('Link'));
+          this.first  = Link["first"];
+          this.last   = Link["last"];
+          this.prev   = Link["prev"];
+          this.next   = Link["next"];
+        }));
+   }
+
 
 }
 
 
 
+//https://www.techiediaries.com/angular-httpclient-headers-full-response/
 
 
 /*
