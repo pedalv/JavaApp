8e31c280d94862bbb343d2a44d24cca00f912188
Pedro Alves <alves.pedro0@gmail.com>
2020-10-06 13:58:31 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/employee/employee.component.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../services/employee.service';
+import { Employee } from '../domain/employee';
 
 @Component({
   selector: 'app-employee',
@@ -8,13 +9,8 @@ import { EmployeeService } from '../services/employee.service';
 })
 export class EmployeeComponent implements OnInit {
 
-  employees = [
-    {id: 1, name: "E 001", description: "E 001 des", email: "e001@email.com"},
-    {id: 2, name: "E 002", description: "E 002 des", email: "e002@email.com"},
-    {id: 3, name: "E 003", description: "E 003 des", email: "e003@email.com"},
-    {id: 4, name: "E 004", description: "E 004 des", email: "e004@email.com"}
-  ];
-  selectedEmployee;
+  employees : Employee[] = [];
+  selectedEmployee : Employee;
 
   constructor(private service: EmployeeService) { }
 
@@ -23,32 +19,28 @@ export class EmployeeComponent implements OnInit {
   }
 
   allEmployees(){
-
-      this.service.allEmployees()
+    this.service.allEmployees()
         .subscribe(
           data => {
             console.log(data);
-            //employees = data;
+            this.employees = data;
           },
           error => {
             console.log(error);
-            console.error("Feil! Hent all ansatte!");
           }
       );
     }
 
+  public selectEmployee(e : Employee){
+      this.selectedEmployee = e;
+    }
 
+  //TODO all update single delete
 
-
-
-  public createEmployee(e: {id, name, description, email}){
+  public createEmployee(e: {id: number, firstName: string, lastName: string, role: string}){
     console.log("Employee created: ", e);
 
     this.employees.push(e);
-  }
-
-  public selectEmployee(e){
-    this.selectedEmployee = e;
   }
 
 }
