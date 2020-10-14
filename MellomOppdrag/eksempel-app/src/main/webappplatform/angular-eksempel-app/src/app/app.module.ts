163d976d7823086fd9cf02dfa414eb7d35eef8d9
Pedro Alves <alves.pedro0@gmail.com>
2020-10-14 16:07:11 +0200

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/app.module.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/app.module.ts
@@ -4,19 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-//import { LoginModule } from './login/login.module';
-//import { LoginComponent } from './users/login/login.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
-import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { KafkaComponent } from './kafka/kafka.component';
 import { PartnerComponent } from './partner/partner.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { CustomerComponent } from './customer/customer.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
@@ -24,18 +19,15 @@ import { FooterComponent } from './footer/footer.component';
     //LoginModule,        // Eager loaded since we may need to go here right away as browser loads based on route user enters
     AppRoutingModule,     // Main routes for application
     CoreModule,           // Singleton objects (services, components that are loaded only once, etc.)
-    SharedModule,        // Shared (multi-instance) objects
+    SharedModule          // Shared (multi-instance) objects
   ],
   declarations: [
     AppComponent,
-    HeaderComponent,
-    NavComponent,
     HomeComponent,
     KafkaComponent,
     PartnerComponent,
     CustomerComponent,
-    EmployeeComponent,
-    FooterComponent,
+    EmployeeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/core/core.module.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/core/core.module.ts
@@ -5,15 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
 
-//import { HeaderComponent } from './header/header.component';
-//import { NavComponent } from './nav/nav.component';
-//import { HomeComponent } from './home/home.component';
-//import { KafkaComponent } from './kafka/kafka.component';
-//import { LoginComponent } from './users/login/login.component';
-//import { PartnerComponent } from './partner/partner.component';
-//import { EmployeeComponent } from './employee/employee.component';
-//import { CustomerComponent } from './customer/customer.component';
-//import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
@@ -23,18 +17,15 @@ import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
   ],
   exports: [
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ],
   declarations: [
-    //HeaderComponent,
-    //NavComponent,
-    //HomeComponent,
-    //KafkaComponent,
-    //LoginComponent,
-    //PartnerComponent,
-    //CustomerComponent,
-    //EmployeeComponent,
-    //FooterComponent
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ],
   providers: [ ] // these should be singleton
 })
