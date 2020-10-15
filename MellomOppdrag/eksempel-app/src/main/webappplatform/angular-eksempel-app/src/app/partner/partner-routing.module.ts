50ac047d94913e3a37d9a7cc9536fb8408a67b00
Pedro Alves <alves.pedro0@gmail.com>
2020-10-15 11:48:44 +0200

    MellomOppdrag

---
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/partner/partner-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PartnerComponent } from './partner.component';
+
+const routes: Routes = [
+  { path: "", component: PartnerComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forChild(routes) ],
+  exports: [ RouterModule ],
+  declarations: [ ]
+})
+export class PartnerRoutingModule {
+  static components = [ PartnerComponent ];
+}
---
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/partner/partner.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { PartnerRoutingModule } from './partner-routing.module';
+
+@NgModule({
+  imports:      [ PartnerRoutingModule ],
+  declarations: [ PartnerRoutingModule.components ]
+})
+export class PartnerModule { }
