f89dafd4366f921f5782aa02211021e41b70d032
Pedro Alves <alves.pedro0@gmail.com>
2020-11-05 23:06:59 +0100

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/store/customers/customers.module.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/store/customers/customers.module.ts
@@ -5,8 +5,16 @@ import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 
+/* NgRx */
+import { StoreModule } from '@ngrx/store';
+
 @NgModule({
-  imports:      [ CommonModule, CustomersRoutingModule ],
+  imports:      [
+    CommonModule,
+    CustomersRoutingModule,
+    //StoreModule.forFeature('products', productReducer)
+    //StoreModule.forFeature('', '')
+  ],
   declarations: [ CustomersRoutingModule.components, CustomersListComponent, CustomerDetailsComponent ]
 })
 export class CustomersModule { }
