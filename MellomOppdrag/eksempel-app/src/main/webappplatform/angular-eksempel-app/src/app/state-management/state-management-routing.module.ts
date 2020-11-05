cf72c4a2acb7c52d319b2c4f8547b28a0bc91492
Pedro Alves <alves.pedro0@gmail.com>
2020-11-05 23:07:31 +0100

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/state-management/state-management-routing.module.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/state-management/state-management-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { StateManagementComponent } from './state-management.component';
+import { CustomersNgrxComponent } from './customers-ngrx/customers-ngrx.component';
 import { NgrxCustomersComponent } from './ngrx/ngrx-customers/ngrx-customers.component';
 import { NgrxDataCustomersComponent } from './ngrx-data/ngrx-data-customers/ngrx-data-customers.component';
 import { ObservableStoreCustomersComponent } from './observable-store/observable-store-customers/observable-store-customers.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
     path: "",
     component: StateManagementComponent,
     children: [
+      { path: 'customers', component: CustomersNgrxComponent },
       { path: 'ngrx', component: NgrxCustomersComponent },
       { path: 'ngrx-data', component: NgrxDataCustomersComponent },
       { path: 'observable-store', component: ObservableStoreCustomersComponent },
