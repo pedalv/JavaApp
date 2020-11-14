0b87dcd280bdca97a0bc859aa7195bc461b63ba6
Pedro Alves <alves.pedro0@gmail.com>
2020-11-14 01:04:17 +0100

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/state/customers/customers.component.spec.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/state/customers/customers.component.spec.ts
@@ -2,13 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomersComponent } from './customers.component';
 
+/* NgRx */
+//import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { StoreModule } from '@ngrx/store';
+import { customerReducer } from './state/customer.reducer';
+//TODO https://ngrx.io/guide/store/testing
+
 describe('CustomersComponent', () => {
   let component: CustomersComponent;
   let fixture: ComponentFixture<CustomersComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomersComponent ]
+      declarations: [ CustomersComponent ],
+      imports: [StoreModule.forRoot({ customers: customerReducer })],
     })
     .compileComponents();
   });
