234c21091ece49c3c6593ca16fe8f1244fe5aaa5
Pedro Alves <alves.pedro0@gmail.com>
2020-12-20 14:56:59 +0100

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/shared/pipe/sort-by-pipe.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/shared/pipe/sort-by-pipe.ts
@@ -11,36 +11,36 @@ import { orderBy,sortBy } from 'lodash';
 })
 export class SortByPipe implements PipeTransform {
 
-  transform(value: any[],caseInsensitive=false, order = '', column: string = ''): any[] {
+  transform(value: any[], caseInsensitive=false, order = '', column: string = ''): any[] {
     if (!value || order === '' || !order) { return value; } // no array
     if (!column || column === '') {
-      const sorted=this.sortOnCaseSensitivity(value,caseInsensitive);
+      const sorted=this.sortOnCaseSensitivity(value, caseInsensitive);
       if(order==='asc'){return sorted}
       else{return sorted.reverse();}
     } // sort 1d array
     if (value.length <= 1) { return value; } // array with only one item
     else{
-      const converted=this.convertMultiOnCaseSensitivity(value,column,caseInsensitive);
+      const converted=this.convertMultiOnCaseSensitivity(value, column, caseInsensitive);
       return orderBy(converted, ['sortCol'], [order]).map(v=>{
         delete v['sortCol'];
         return v;
       });
     }
   }
-  sortOnCaseSensitivity(value:any[],caseInsensitive:boolean){
-    return sortBy(value,(v)=>{
-        if(typeof v==='string'&&caseInsensitive){
+  sortOnCaseSensitivity(value:any[], caseInsensitive: boolean){
+    return sortBy(value, (v)=>{
+        if(typeof v==='string' && caseInsensitive){
         return v.toLowerCase()
         }
         return v;
       });
   }
-  convertMultiOnCaseSensitivity(value:any[],column,caseInsensitive){
+  convertMultiOnCaseSensitivity(value:any[], column : string, caseInsensitive: boolean){
     let converted=value;
       if(caseInsensitive){
         converted=value.map(v=>{
           if(typeof v[column]==='string'){
-          return {...v,sortCol:v[column].toLowerCase()}
+          return {...v, sortCol:v[column].toLowerCase()}
         }
         return v;
         })
