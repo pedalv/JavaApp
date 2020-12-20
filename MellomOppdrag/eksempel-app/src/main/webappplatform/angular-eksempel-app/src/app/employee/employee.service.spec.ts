4ceb433e7ef7aaf1bb01e6d44949813220bd0e3f
Pedro Alves <alves.pedro0@gmail.com>
2020-12-20 15:51:41 +0100

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/employee/employee.service.spec.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/employee/employee.service.spec.ts
@@ -7,8 +7,10 @@ import { HttpClient } from '@angular/common/http';
 
 import { EmployeeService } from './employee.service';
 import { Employee } from '../domain/employee';
+import {Observable} from "rxjs";
 
 describe('HttpClient testing', () => {
+  //sytem under test
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
 
@@ -24,13 +26,20 @@ describe('HttpClient testing', () => {
   });
 
   it('works', () => {
+    // arrange
+
+    // act
+
+    // assert
+    expect(httpClient).toBeTruthy();
+    expect(httpTestingController).toBeTruthy();
   });
 });
 
 
 describe('EmployeeService', () => {
+  //sytem under test
   let service: EmployeeService;
-  //let httpClient: HttpTestingController;
   let httpMock: HttpTestingController;
 
   beforeEach(() => {
@@ -43,40 +52,99 @@ describe('EmployeeService', () => {
   });
 
   it('should be created', () => {
+    // arrange
+
+    // act
+
+    // assert
     expect(service).toBeTruthy();
   });
 
   it(`should fetch all employees as an Observable`, async( inject(
+
     [HttpTestingController, EmployeeService],
     (httpClient: HttpTestingController, empservice: EmployeeService) => {
 
-        const employeesStub = [
-          {
-            "id": 10,
-            "firstName":"P",
-            "lastName":"M",
-            "role":"Seniorutvikler"
-          },
-          {
-            "id": 11,
-            "firstName":"L",
-            "lastName":"A",
-            "role":"Leder"
-          }
-        ];
-
-        empservice.allEmployees()
-          .subscribe((employees : Employee[]) => {
-            expect(employees.length).toBe(2);
-          });
-
-          let req = httpMock.expectOne('/api/employee/all');
-          expect(req.request.method).toBe("GET");
-
-          req.flush(employeesStub);
-          httpMock.verify();
-
+      // arrange
+      const employeesStub = [
+        {
+          "id": 10,
+          "firstName":"P",
+          "lastName":"M",
+          "role":"Seniorutvikler"
+        },
+        {
+          "id": 11,
+          "firstName":"L",
+          "lastName":"A",
+          "role":"Leder"
         }
+      ];
+
+
+      // act
+      empservice.allEmployees()
+        .subscribe((employees : Employee[]) => {
+          expect(employees.length).toBe(2);
+        });
+
+      // assert
+      let req = httpMock.expectOne('/api/employee/all');
+      expect(req.request.method).toBe("GET");
+
+      req.flush(employeesStub);
+      httpMock.verify();
+
+    }
     )));
+
+
+  it(`should create employee`, async( inject(
+    [HttpTestingController, EmployeeService],
+    (httpClient: HttpTestingController, empservice: EmployeeService) => {
+
+      // arrange
+      const employeeCreated= {
+          "id": 0,
+          "firstName":"P",
+          "lastName":"M",
+          "role":"Seniorutvikler"
+        };
+
+      //MOCK TODO return this.httpClient.post(this.apiURL + '/add', obj, this.headers);
+
+      // act
+      let result: Observable<Object> = empservice.createEmployee(employeeCreated);
+
+      // assert
+      result.subscribe((employees : Employee) => {
+        expect(employees.id).toBe(0);
+        expect(employees.firstName).toBe('P');
+        expect(employees.lastName).toBe('M');
+        expect(employees.role).toBe('Seniorutvikler');
+      });
+
+      let req = httpMock.expectOne('/api/employee/add');
+      expect(req.request.method).toBe("POST");
+      req.flush(employeeCreated);
+      httpMock.verify();
+    }
+  )));
+
+
+
+
+
+
+
+
+  //TODO
+  //empservice.replaceEmployee()
+  //empservice.singleEmployee()
+  //empservice.deleteEmployee()
+  //empservice.getFirstPage()
+  //empservice.getNextPage
+
+
 
 });
