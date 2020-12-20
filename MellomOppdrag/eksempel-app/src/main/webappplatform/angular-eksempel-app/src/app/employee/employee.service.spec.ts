41dbcb23a51c89f2df3e5079b2b96ac31e441c44
Pedro Alves <alves.pedro0@gmail.com>
2020-12-20 17:43:45 +0100

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/employee/employee.service.spec.ts
+++ MellomOppdrag/eksempel-app/src/main/webappplatform/angular-eksempel-app/src/app/employee/employee.service.spec.ts
@@ -9,6 +9,7 @@ import { EmployeeService } from './employee.service';
 import { Employee } from '../domain/employee';
 import {Observable} from "rxjs";
 
+
 describe('HttpClient testing', () => {
   //sytem under test
   let httpClient: HttpClient;
@@ -34,6 +35,60 @@ describe('HttpClient testing', () => {
     expect(httpClient).toBeTruthy();
     expect(httpTestingController).toBeTruthy();
   });
+});
+
+
+describe('EmployeeService', () => {
+  let service: EmployeeService;
+  //let httpClient: HttpTestingController;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers : [ EmployeeService ]
+    });
+    service = TestBed.inject(EmployeeService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it(`should fetch all employees as an Observable`, async( inject(
+    [HttpTestingController, EmployeeService],
+    (httpClient: HttpTestingController, empservice: EmployeeService) => {
+
+      const employeesStub = [
+        {
+          "id": 10,
+          "firstName":"P",
+          "lastName":"M",
+          "role":"Seniorutvikler"
+        },
+        {
+          "id": 11,
+          "firstName":"L",
+          "lastName":"A",
+          "role":"Leder"
+        }
+      ];
+
+      empservice.allEmployees()
+        .subscribe((employees : Employee[]) => {
+          expect(employees.length).toBe(2);
+        });
+
+      let req = httpMock.expectOne('/api/employee/all');
+      expect(req.request.method).toBe("GET");
+
+      req.flush(employeesStub);
+      httpMock.verify();
+
+    }
+  )));
+
 });
 
 
