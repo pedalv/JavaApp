70cd7454f95d2582f8355fcab3d5728160ae4a45
Pedro Alves <alves.pedro0@gmail.com>
2021-02-04 09:53:19 +0100

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/webappuicomponent/my-app/src/App.tsx
+++ MellomOppdrag/eksempel-app/src/main/webappuicomponent/my-app/src/App.tsx
@@ -40,38 +40,6 @@ class App extends React.Component<object, CounterSate> {
 }
 
 /*
-class App extends React.Component {
-    state = {
-        profiles: [],
-    };
-    addNewProfile = (profileData) => {
-        this.setState(prevState => ({
-            profiles: [...prevState.profiles, profileData],
-        }));
-    };
-    render() {
-        return (
-            <div>
-                <div className="header">{this.props.title}</div>
-                <Form onSubmit={this.addNewProfile} />
-                <CardList profiles={this.state.profiles} />
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(
-    <App title="The GitHub Cards App" />,
-    mountNode,
-);
-
-
-
- */
-
-
-
-/*
 function App() {
     return (
         <div>
