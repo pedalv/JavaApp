69d814cf62cec689c77f37af793d4202392f1d78
Pedro Alves <alves.pedro0@gmail.com>
2021-02-05 10:57:16 +0100

    MellomOppdrag

--- MellomOppdrag/eksempel-app/src/main/java/no/agitec/fagaften/mellom/oppdrag/TesteInner.java
+++ MellomOppdrag/eksempel-app/src/main/java/no/agitec/fagaften/mellom/oppdrag/TesteInner.java
@@ -13,12 +13,33 @@ public class TesteInner {
     Boolean temp = true;
 
     public TesteInner(){
-        new Inner_Demo().print();
+        try {
+            new Inner_Demo().print();
+        } catch (Exception e) {
+            //
+        } finally {
+            System.out.println("finaly");
+        }
     }
 
     public static void main(String... args) {
         new TesteInner();
-        System.out.println("End");
     }
 
 }
+
+
+    /*
+
+        //https://stackoverflow.com/questions/27086377/get-previous-and-next-row-from-rows-selected-with-where-conditions
+
+        SELECT a.name, b.name
+        FROM   Language a
+        WHERE  ID IN(SELECT ID + 1
+                     FROM   Language b
+                     ORDER BY a.salaru desc) -- will fetch next rows
+                AND a.Salary > b.Salary	-- compare salary
+        order by a.salary desc
+        ;
+
+     */
