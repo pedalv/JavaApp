bfcb0a102913702226af4aba61c90e4ba442f599
Pedro Alves <Pedro.Alves@dfo.no>
2021-04-16 15:20:15 +0200

    CloudComputer

---
+++ CloudComputing/spring-boot-docker-gradle/src/main/java/no/dfo/springbootdockergradle/SpringBootDockerGradleApplication.java
@@ -0,0 +1,13 @@
+package no.dfo.springbootdockergradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerGradleApplication.class, args);
+	}
+
+}
---
+++ CloudComputing/spring-boot-docker-gradle/src/test/java/no/dfo/springbootdockergradle/SpringBootDockerGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package no.dfo.springbootdockergradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
