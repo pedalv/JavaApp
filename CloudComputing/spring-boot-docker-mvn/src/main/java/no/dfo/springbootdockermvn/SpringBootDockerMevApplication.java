8096ae8c0aff350126b17be11315d09a94b41cef
Pedro Alves <alves.pedro0@gmail.com>
2021-04-16 13:11:05 +0200

    CloudComputing

--- CloudComputing/spring-boot-docker-mvn/src/main/java/no/dfo/springbootdockermvn/SpringBootDockerMevApplication.java
+++ CloudComputing/spring-boot-docker-mvn/src/main/java/no/dfo/springbootdockermvn/SpringBootDockerMevApplication.java
@@ -17,6 +17,7 @@ public class SpringBootDockerMevApplication {
 	java -jar target/spring-boot-docker-mvn-0.0.1-SNAPSHOT.jar
 
 	docker build -t pedalv/spring-boot-docker .
+  	docker run -p 8080:8080 pedalv/spring-boot-docker
 
 
 	https://stackoverflow.com/questions/53375613/why-is-the-java-11-base-docker-image-so-large-openjdk11-jre-slim
