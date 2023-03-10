fce12b975c1e2fdfb346b29444b00a209186cd65
Pedro Alves <Pedro.Alves@dfo.no>
2023-03-10 14:09:52 +0100

    Added functional-programming project

---
+++ CloudComputing/functional-programming/src/test/java/no/dfo/fp/functionalprogramming/FunctionalProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package no.dfo.fp.functionalprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
