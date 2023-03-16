0918f7f87fce4844e27f0fc9e48e788db6cdb2ce
Pedro Alves <Pedro.Alves@dfo.no>
2023-03-16 10:28:53 +0100

    Checked later where to find Result class from example for code factoring exception and success result from API

---
+++ CloudComputing/functional-programming/src/main/java/no/dfo/fp/functionalprogramming/streamsadvances/functionreactive/ResultInAction.java
@@ -0,0 +1,23 @@
+package no.dfo.fp.functionalprogramming.streamsadvances.functionreactive;
+
+import javax.xml.transform.Result;
+
+/**
+ * https://developer.paypal.com/braintree/docs/reference/general/result-objects
+ * https://noraui.github.io/NoraUi/com/github/noraui/exception/Result.Success.html
+ * https://www.tabnine.com/code/java/packages/com.opengamma.strata.collect.result
+ * https://www.tabnine.com/code/java/classes/com.fpinjava.common.Result
+ */
+public class ResultInAction {
+
+    void check() {
+
+
+        Result customerResult = null;
+
+
+
+    }
+
+
+}
