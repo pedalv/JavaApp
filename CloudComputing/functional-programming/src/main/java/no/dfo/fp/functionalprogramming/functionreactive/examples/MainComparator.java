f78af30ef9f8431dd4090ab394cca3286b0304da
Pedro Alves <Pedro.Alves@dfo.no>
2023-03-16 16:40:58 +0100

    Fixed sorted for streams

--- CloudComputing/functional-programming/src/main/java/no/dfo/fp/functionalprogramming/functionreactive/examples/MainComparator.java
+++ CloudComputing/functional-programming/src/main/java/no/dfo/fp/functionalprogramming/functionreactive/examples/MainComparator.java
@@ -1,20 +1,27 @@
 package no.dfo.fp.functionalprogramming.functionreactive.examples;
 
-import no.dfo.fp.functionalprogramming.functionreactive.funint.Comparator;
+//import no.dfo.fp.functionalprogramming.functionreactive.funint.Comparator;
 import no.dfo.fp.functionalprogramming.model.Person;
+import no.dfo.fp.functionalprogramming.model.Product;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainComparator {
 
     public static void main(String... args) {
 
-        java.util.Comparator<String> comparatorlambda = (s1, s2) -> Integer.compare(s1.length(), s2.length());
+        Comparator<String> comparatorlambda = (s1, s2) -> Integer.compare(s1.length(), s2.length());
         String[] names = new String[]{ "PP", "PPP", "P" };
-        //Arrays.sort(names, comparator);
-        Arrays.sort(names, comparatorlambda);
+        Arrays.sort(names, comparatorlambda); //ok
 
         //--
 
@@ -33,35 +40,42 @@ public class MainComparator {
                 .thenComparing(Person::getFirstName)
                 .thenComparing(Person::getAge);
 
-        Person p1 = new Person("Alice", "lastALI ", 23);
-        Person p2 = new Person("Brian", "lastBRI", 56);
-        Person p3 = new Person("Chelsea", "lastCHEL", 46);
-        Person p4 = new Person("David", "lastDAV", 28);
-        Person p5 = new Person("Erica", "lastERI", 37);
+        Person p1 = new Person("David", "lastDAV", 28);
+        Person p2 = new Person("Erica", "lastERI", 37);
+        Person p3 = new Person("Alice", "lastALI ", 23);
+        Person p4 = new Person("Brian", "lastBRI", 56);
+        Person p5 = new Person("Chelsea", "lastCHEL", 46);
         Person p6 = new Person("Francisco", "lastFRA", 18);
+        Person p7 = new Person("Francisco", "lastFRA", 17);
 
-
-        //TODO - How use the new Comparator?
+        //******************IMPORTANT*****************
         //Stream<T> sorted(Comparator<? super T> comparator);
-        Stream<Person> persons = Stream.of(p1,p2,p3,p4,p5,p6);
-        //persons.sorted(cmp);
-        //persons.sorted(cmpPersonAge);
-        //persons.sorted(cmpPersonLastName);
-
-        //persons = persons.sorted(cmp);
+        Stream<Person> persons = Stream.of(p1,p2,p3,p4,p5,p6,p7);
+        persons
+                .sorted(cmp)
+                //.sorted(cmpPersonAge)
+                //.sorted(cmpPersonLastName)
+                //.sorted(cmpAge)
+                .forEach(p -> System.out.println(p.toString())); //ok
+        //Collections.sort(persons, cmp); //  no suitable method found for sort(Person[],no.dfo.fp.functionalprogramming.functionreactive.funint.Comparator<Person>)
 
         //public static <T> void sort(T[] a, Comparator<? super T> c) { ... }
         Person[] personsa = new Person[]{ p1,p2,p3,p4,p5,p6 };
-        //Arrays.sort(personsa, cmp);
-        //Arrays.sort(personsa, cmpPersonAge);
-        //Arrays.sort(personsa, cmpPersonLastName);
+        Arrays.sort(personsa, cmp);
+        Arrays.sort(personsa, cmpPersonAge);
+        Arrays.sort(personsa, cmpPersonLastName);
+        //Collections.sort(personsa, cmp); //  no suitable method found for sort(Person[],no.dfo.fp.functionalprogramming.functionreactive.funint.Comparator<Person>)
+
+        List<Person> persons2 = new ArrayList<>(List.of(p1, p2, p3, p4)); // mutable list
+        persons2.forEach(p -> System.out.println(p.toString()));
+        persons2.sort(cmp);   //OK          // incompatible types:
+        persons2.forEach(p -> System.out.println(p.toString()));
+        persons2.sort(cmpAge); //OK
+        Collections.sort(persons2, cmp); //OK
 
 
-
-
-
-
-
+        List<Person> persons3 = Arrays.asList(p1, p2, p3, p4); // cant add or remove
+        persons3.sort(cmp); //OK
 
     }
 
--- CloudComputing/functional-programming/src/main/java/no/dfo/fp/functionalprogramming/functionreactive/funint/Comparator.java
+++ CloudComputing/functional-programming/src/main/java/no/dfo/fp/functionalprogramming/functionreactive/funint/Comparator.java
@@ -7,18 +7,15 @@ public interface Comparator<T> {
 
     int compare(T t1, T t2);
 
-    default Comparator<T> thenComparing(Comparator<T> cmp) {
-
+    default Comparator<T> thenComparing(Comparator<? super T> cmp) {
         return (p1, p2) -> compare(p1, p2) == 0 ? cmp.compare(p1, p2) : compare(p1, p2) ;
     }
 
-    default Comparator<T> thenComparing(Function<T, Comparable> f) {
-
+    default Comparator<T> thenComparing(Function<? super T, Comparable> f) {
         return thenComparing(comparing(f)) ;
     }
 
     static <U> Comparator<U> comparing(Function<U, Comparable> f) { //Comparable for Integer and String
-
         return (p1, p2) ->  f.apply(p1).compareTo(f.apply(p2));
     }
 
