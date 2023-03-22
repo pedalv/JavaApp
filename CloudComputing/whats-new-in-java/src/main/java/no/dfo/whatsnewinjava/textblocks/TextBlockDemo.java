6eaf940bb4a93b28eb407ce412debc24c2d9c0cd
Pedro Alves <Pedro.Alves@dfo.no>
2023-03-22 15:11:49 +0100

    Added Text Blocks with translateEscapes and  formatted examples

--- CloudComputing/whats-new-in-java/src/main/java/no/dfo/whatsnewinjava/textblocks/TextBlockDemo.java
+++ CloudComputing/whats-new-in-java/src/main/java/no/dfo/whatsnewinjava/textblocks/TextBlockDemo.java
@@ -1,9 +1,14 @@
 package no.dfo.whatsnewinjava.textblocks;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Consumer;
 
 /**
  * java --enable-preview --source 12 TextBlockDemo.java
+ *
+ * text.translateEscapes()
+ *
  */
 public class TextBlockDemo {
 
@@ -16,10 +21,39 @@ public class TextBlockDemo {
                 string""";
         System.out.println(text);
         showWhitespace(text);
+        textTranslateEscapes();
     }
 
     private static void showWhitespace(String string) {
         System.out.println(string.replaceAll(" ", ".").replaceAll("\n", "\\\\n\n"));
+    }
+
+    private static void textTranslateEscapes() {
+        //String text = Files.readString(Path.of("\"Hello\tWorld\"\n"));
+
+        String text = "\"Hello\tWorld\"\n";
+        System.out.println(text);
+        //****IMPORTANT****
+        System.out.println(text.translateEscapes());
+
+        //Old
+        System.out.println(String.format("""
+               <html>
+                <head><head>
+                <body>%s</body>
+               </html>
+               """, "HELLO"));
+
+        //new
+        //****IMPORTANT****
+        System.out.println("""
+               <html>
+                <head><head>
+                <body>%s</body>
+               </html>
+               """.formatted("Hello"));
+
+
     }
 
 }
