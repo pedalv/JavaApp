29b7fb41e86a198045a2f0ead23953aee08b49a7
Pedro Alves <Pedro.Alves@dfo.no>
2023-04-28 08:55:49 +0200

    Introduction Java 9

---
+++ CloudComputing/whats-new-in-java/src/main/java/no/dfo/whatsnewinjava/module/langandlib/optional/OptionalOr.java
@@ -0,0 +1,31 @@
+package no.dfo.whatsnewinjava.module.langandlib.optional;
+
+import no.dfo.whatsnewinjava.module.langandlib.Book;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+public class OptionalOr {
+    public static void main(String... args) {
+        Optional<Book> localFallback = Optional.of(Book.getBook());
+
+        // Before Optional.or
+        Book bestBookBefore = getBestOffer()
+                .orElse(getExternalOffer().orElse(localFallback.get()));  // .get() is BAD!
+
+        Optional<Book> bestBook =
+                getBestOffer()
+                .or(() -> getExternalOffer())
+                .or(() -> localFallback);
+        System.out.println(bestBook);
+    }
+
+    static Optional<Book> getBestOffer() {
+        return Optional.empty();
+    }
+
+    static Optional<Book> getExternalOffer() {
+        return Optional.of(new Book("External Book", Set.of(), 11.99));
+    }
+}
