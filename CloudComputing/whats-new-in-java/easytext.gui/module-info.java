251e4831a8b670c20762923157cf759508dfd70f
Pedro Alves <Pedro.Alves@dfo.no>
2023-05-02 16:13:54 +0200

    Introduction Java 9 modularity: first look

--- CloudComputing/whats-new-in-java/easytext.gui/module-info.java
+++ CloudComputing/whats-new-in-java/easytext.gui/module-info.java
@@ -2,6 +2,7 @@ module easytext.gui {
     // JavaFX needs to instantiate our Main Application class
     exports no.dfo.javamodularity.easytext.gui to javafx.graphics;
 
+    //requires javafx.graphics;
     requires javafx.controls;
     requires easytext.analysis;
 }
