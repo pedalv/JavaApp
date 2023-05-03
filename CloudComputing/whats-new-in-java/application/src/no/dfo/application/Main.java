84f524ea44f4852830bdc631c39a5f27006753dd
Pedro Alves <Pedro.Alves@dfo.no>
2023-05-03 09:16:36 +0200

    Introduction Java 9 modularity: first look

--- CloudComputing/whats-new-in-java/application/src/no/dfo/application/Main.java
+++ CloudComputing/whats-new-in-java/application/src/no/dfo/application/Main.java
@@ -1,6 +1,24 @@
 package no.dfo.application;
 
 import org.apache.commons.lang3.StringUtils;
+
+/**
+ * https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+ * - fra Mar 02, 2021
+ * // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+ * implementation 'org.apache.commons:commons-lang3:3.12.0'
+ *
+ * // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+ * implementation group: 'org.apache.commons', name: 'commons-lang3', version: '3.12.0'
+ *
+ * <!-- https://mvnrepository.com/artifact/org.apache.commons/commons-lang3 -->
+ * <dependency>
+ *     <groupId>org.apache.commons</groupId>
+ *     <artifactId>commons-lang3</artifactId>
+ *     <version>3.12.0</version>
+ * </dependency>
+ *
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
