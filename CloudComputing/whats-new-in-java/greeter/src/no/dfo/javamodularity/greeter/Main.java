409873b9e43e3432fe1d8fe9a0693c8503fd8d30
Pedro Alves <Pedro.Alves@dfo.no>
2023-05-03 15:06:05 +0200

    Introduction Java 9 modularity: first look

--- CloudComputing/whats-new-in-java/greeter/src/no/dfo/javamodularity/greeter/Main.java
+++ CloudComputing/whats-new-in-java/greeter/src/no/dfo/javamodularity/greeter/Main.java
@@ -13,6 +13,21 @@ package no.dfo.javamodularity.greeter;
  * rm -rf out # Optional to clear previous compilation results
  *
  * # -d sets output directory
+ * SINGLE MODULE:
+ * javac -d {dir} {all source files, including module-info.java}
+ * javac -d out \
+ * .\greeter\src\no\dfo\javamodularity\greeter\Main.java \
+ * .\greeter\src\module-info.java
+ *
+ * MULTIPLE MODULES:
+ * javac -d {dir} \
+ *  --module-source-pat {src_dir} \
+ *  -m {module_name},{module_name}
+ * javac -d out \
+ *  --module-source-path src \
+ *  --module greeter
+ *
+ *
  * ..\..\..\..\Java\jdk-17.0.6+10\bin\javac -d out .\greeter\src\no\dfo\javamodularity\greeter\Main.java .\greeter\src\module-info.java
  *
  * ..\..\..\..\..\Java\jdk-17.0.6+10\bin\javac -d out .\src\no\dfo\javamodularity\greeter\Main.java .\src\module-info.java
@@ -20,6 +35,17 @@ package no.dfo.javamodularity.greeter;
  * javac -d out src/com/javamodularity/greeter/Main.java src/module-info.java
  *
  * # -p sets the module path, -m the initial module to run
+ * Running A MODULE:
+ * java -p {module_path} \
+ *      -m {module}/{fully qualified main class}
+ *
+ * java --module-path {module_path} \
+ *      --module {module}/{fully qualified main class}
+ *
+ * java  --module-path out \
+ *       --module greeter/com.javamodularity.greeter.Main
+ *
+ *
  * ..\..\..\..\Java\jdk-17.0.6+10\bin\java -p out -m greeter/no.dfo.javamodularity.greeter.Main
  * Greetings from a module!
  *
