23a090eacc8dd8e17f1a55aeaafda82fe5f17ae9
Pedro Alves <Pedro.Alves@dfo.no>
2023-05-03 16:29:33 +0200

    Introduction Java 9 modularity: first look

--- CloudComputing/whats-new-in-java/greeter.cli/src/greeter/cli/Main.java
+++ CloudComputing/whats-new-in-java/greeter.cli/src/greeter/cli/Main.java
@@ -4,16 +4,29 @@ package greeter.cli;
 import greeter.hello.service.HelloMessageService;
 
 /**
+ * PS C:\workspace2\JavaApp\CloudComputing\whats-new-in-java>
  * ..\..\..\..\..\Java\jdk-17.0.6+10\bin\javac -d out --module-source-path src -m greeter.cli,greeter.hello
+ * error: module greeter.cli not found in module source path
+ * error: module greeter.hello not found in module source path
  *
+ *
+ * PS C:\workspace2\JavaApp\CloudComputing\whats-new-in-java\greeter.cli>
  * ..\..\..\..\..\Java\jdk-17.0.6+10\bin\javac -d out --module-source-path src -m greeter.cli
+ * error: module greeter.cli not found in module source path
  * ..\..\..\..\..\Java\jdk-17.0.6+10\bin\javac -d out .\src\greeter\cli\Main.java .\src\module-info.java
+ * .\src\module-info.java:2: error: module not found: greeter.hello
+ *     requires greeter.hello;
+ *                     ^
+ * 1 error
  *
  *
+ * PS C:\workspace2\JavaApp\CloudComputing\whats-new-in-java\greeter.hello>
  * ..\..\..\..\..\Java\jdk-17.0.6+10\bin\javac -d out --module-source-path src -m greeter.hello
+ * error: module greeter.hello not found in module source path
  * ..\..\..\..\..\Java\jdk-17.0.6+10\bin\javac -d out \
  *  .\src\greeter\hello\service\HelloMessageService.java \
  *  .\src\greeter\helloutil\CapitalizeUtil.java .\src\module-info.java
+ *
  */
 public class Main {
     public static void main(String[] args) {
